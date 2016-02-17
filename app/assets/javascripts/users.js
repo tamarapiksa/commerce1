cbd8ea752736984651114edb6aaa6c0175e17f34
Tamara Piksa <tamara.piksa@gmail.com>
2016-02-16 23:17:05 -0800

    Changed var name in javascripts

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,15 +1,17 @@
-$(document).ready(function(){
+$(document).ready(function() {
   Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
-  $('#form-submit-btn').click(function(event){
+  // Watch for a form submission:
+  $("#form-submit-btn").click(function(event) {
     event.preventDefault();
     $('input[type=submit]').prop('disabled', true);
-    var error = false,
+    var error = false;
     var ccNum = $('#card_number').val(),
-    var cvcNum = $('#card_code').val(),
-    var expMonth = $('#card_month').val(),
-    var expYear = $('#card_year').val();
+        cvcNum = $('#card_code').val(),
+        expMonth = $('#card_month').val(),
+        expYear = $('#card_year').val();
 
     if (!error) {
+      // Get the Stripe token:
       Stripe.createToken({
         number: ccNum,
         cvc: cvcNum,
@@ -18,12 +20,20 @@ $(document).ready(function(){
       }, stripeResponseHandler);
     }
     return false;
-  });
-
-  function stripeResponseHandler(status, response){
+  }); // form submission
+  
+  function stripeResponseHandler(status, response) {
+    // Get a reference to the form:
     var f = $("#new_user");
+
+    // Get the token from the response:
     var token = response.id;
+
+    // Add the token to the form:
     f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
-    f.get(0).submit();
+
+    // Submit the form:
+    f.get(0).submit(); 
   }
 });
+
