2969632fec44d2d7c73dd7db1761287b63c485f8
Tamara Piksa <tamara.piksa@gmail.com>
2016-03-09 19:16:53 -0800

    Button colors and div backgrounds

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,11 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+
+
+window.setTimeout(function() {
+    $(".alert").fadeTo(500, 0).slideUp(500, function(){
+        $(this).remove(); 
+    });
+}, 2000);
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,10 @@
 module UsersHelper
   def job_title_icon
-    if @user.profile.job_title == "Developer"
+    if @user.profile.job_title == "Style Seeker"
       "<i class='fa fa-code'></i>".html_safe
-    elsif @user.profile.job_title == "Entrepreneur"
+    elsif @user.profile.job_title == "Stylist"
       "<i class='fa fa-lightbulb-o'></i>".html_safe
-    elsif @user.profile.job_title == "Investor"
+    elsif @user.profile.job_title == "Seller"
       "<i class='fa fa-dollar'></i>".html_safe
     end
   end
