6267d192edd1002c0f0c31c15f951742ae440d05
Tamara Piksa <tamara.piksa@gmail.com>
2016-03-10 00:19:03 -0800

    Seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+User.create(plan_id: 1)
+Profile.create(first_name: "Jessa", last_name: "Patrick")
